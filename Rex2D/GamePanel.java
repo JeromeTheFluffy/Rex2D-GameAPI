dff98be723a599847b362d23143d238a63961177
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-05-11 10:04:00 -0600

    Create GamePanel.java

---
+++ Rex2D/GamePanel.java
@@ -0,0 +1,16 @@
+package Rex2D;
+
+import Rex2D.Background;
+
+import javax.swing.JPanel; // GamePanel extends JPanel
+
+import java.awt.Image;
+import java.awt.Graphics2D;
+
+class GamePanel extends JPanel {
+        
+        // constructor
+        public GamePanel()
+        {
+                
+                
