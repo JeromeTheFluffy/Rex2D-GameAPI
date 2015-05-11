365d9bbc0ab93baa4ddaeef058a69604146cba7a
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-05-11 10:21:08 -0600

    Update Sprite.java

--- Rex2D/Sprite.java
+++ Rex2D/Sprite.java
@@ -1,7 +1,19 @@
 package Rex2D;
 
 import java.awt.*;
-
+import java.lang.String;
 class Sprite {
+    
+    public int x;
+    public int y;
+    Image sprite;
+    
+    // constructor
+    public Sprite(String imagePath){
+        
+        this.setSprite(imagePath);
+        
+    }
+    // getters and setters
     
 }
